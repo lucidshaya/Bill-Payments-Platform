e53f35028e27fd3afe2c1ec5dd67c58dc89bab24
Ohine.dev <126261826+lucidshaya@users.noreply.github.com>
2025-05-22 13:15:46 +0100

    fix 2

---
+++ frontend/src/pages/comingsoon.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Comingsoon = () => {
+  return (
+    <div>
+        <h1 className='text-lg 7xl'>
+            COMING SOON!
+        </h1>
+    </div>
+  )
+}
+
+export default Comingsoon
